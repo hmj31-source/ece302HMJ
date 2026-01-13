27d09d9edecc8a313cd3c4c4f8a9292d0d22f6eb
Harrison <hmj31@pitt.edu>
2026-01-13 09:42:21 -0500

    added test cases and fixed add.tpp

--- add/test_add.cpp
+++ add/test_add.cpp
@@ -12,3 +12,24 @@ TEST_CASE("Test add", "[Add]"){
 }
 
 /* Your tests here */
+
+TEST_CASE("Test string add", "[Add]"){
+
+  Add<std::string> a;
+  REQUIRE(a.call("one","two")== "onetwo");
+}
+
+TEST_CASE("Test double add", "[Add]"){
+
+  Add<double> a;
+  REQUIRE(a.call(1.5,2.5)==4);
+}
+
+TEST_CASE("Test bool add", "[Add]"){
+
+  Add<bool> a;
+  REQUIRE(a.call(false, false)== false);
+  REQUIRE(a.call(false, true)== true);
+  REQUIRE(a.call(true, true)== true);
+
+}
