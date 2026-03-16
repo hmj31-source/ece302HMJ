a6fa8e795ca3eefd70c2278d81c9e9220cfa9c29
Harrison <hmj31@pitt.edu>
2026-03-16 14:40:03 -0400

    added helpers to start of XMLParser

--- Project 3/project3/XMLParser.cpp
+++ Project 3/project3/XMLParser.cpp
@@ -1,5 +1,63 @@
 #include "XMLParser.hpp"
 
+#include "XMLParser.hpp"
+
+#include <cctype>
+#include <stdexcept>
+
+namespace
+{
+	std::string trim(const std::string& s)
+	{
+		size_t start = 0;
+		while (start < s.size() && std::isspace(static_cast<unsigned char>(s[start])))
+		{
+			start++;
+		}
+
+		size_t end = s.size();
+		while (end > start && std::isspace(static_cast<unsigned char>(s[end-1])))
+		{
+			end--;
+		}
+
+		return s.substr(start, end - start);
+	}
+
+	bool isValidNameStart(char c)
+	{
+		unsigned char uc = static_cast<unsigned char>(c);
+		return std::isalpha(uc) || c == "_" || c ==":";
+	}
+
+	bool isValidNameChar(char c)
+	{
+		unsigned char uc = static_cast<unsigned char>(c);
+		return std::isalnum(uc) || c == "_" || c ==":" || c == "-" ||c == ".";
+	}
+	bool isValidTagNme(const std::string& name)
+	{
+		if (name.empty())
+		{
+			return false;
+		}
+
+		if (!isValidNameStart(name[0]))
+		{
+			return false;
+		}
+
+		for (char c : name)
+		{
+			if (!isValidNameChar(c))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
 XMLParser::XMLParser()
 {
 	// TODO
