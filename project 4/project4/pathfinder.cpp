c8abfb75ee910580dcae83049c496eb287ff6ab0
Harrison <hmj31@pitt.edu>
2026-04-06 10:56:27 -0400

    Made fixes here

--- project 4/project4/pathfinder.cpp
+++ project 4/project4/pathfinder.cpp
@@ -81,7 +81,11 @@ int main(int argc, char *argv[])
   auto isBorder = [&](int r, int c) {
     return r == 0 || r == height - 1 || c ==0 || c == width -1;
   };
-
+  if (isBorder(start.row, start.col)) {
+    image(start.row, start.col) = GREEN;
+    std::cout << "Solution Found" << std::endl;
+  }
+  
   else{
     //bfs seutp
     Queue<State, List<State>> frontier;
