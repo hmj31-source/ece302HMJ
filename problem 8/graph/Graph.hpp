9da012d2dd2f07e2fa8ce4e2adcbf56dbb349de5
Harrison <hmj31@pitt.edu>
2026-04-20 15:31:50 -0400

    Added map and set and other private variables ot Graph.hpp

--- problem 8/graph/Graph.hpp
+++ problem 8/graph/Graph.hpp
@@ -1,6 +1,8 @@
 #ifndef _GRAPH_HPP_
 #define _GRAPH_HPP_
 
+#include <map>
+#include <set>
 #include "AbstractGraph.hpp"
 
 template <typename LabelType>
@@ -22,6 +24,13 @@ class Graph: public AbstractGraph<LabelType>
 
         void breadthFirstTraversal(LabelType start, void visit(LabelType&));
   
+    //added private section
+    private:
+        //map and set
+        std::map<LabelType;
+        std::set<LabelType>> adjList;
+        //int for edged
+        int edgeCount =0;
 };
 
 #include "Graph.tpp"
